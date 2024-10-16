6d8273f94007512157f0c1443e5e90a157d4975f
Maksim Torgashin <soulscavenger@yandex.ru>
2024-10-17 01:48:50 +0300

    fix comment rev_1

--- extensions.py
+++ extensions.py
@@ -5,13 +5,17 @@ class InvalidTokenError(Exception):
     """Кастомное исключения для Токенов."""
 
 
-class SendMessageError(Exception):
-    """Кастомное исключения для отправки сообщений."""
-
-
 class InvalidHomeWorkStatus(Exception):
     """Кастомное исключения для статуса ДЗ."""
 
 
 class InvalidHomeWorkName(Exception):
     """Кастомное исключения для имени ДЗ."""
+
+
+class InvalidStatusCode(Exception):
+    """Кастомное исключения для кода получения API запроса."""
+
+
+class APIRequestError(Exception):
+    """Кастомное исключения для API запроса."""
--- homework.py
+++ homework.py
@@ -6,13 +6,15 @@ import time
 from http import HTTPStatus
 
 from dotenv import load_dotenv
-from telebot import TeleBot
+from telebot import TeleBot, apihelper
+
 
 from extensions import (
     InvalidTokenError,
     InvalidHomeWorkName,
     InvalidHomeWorkStatus,
-    SendMessageError,
+    InvalidStatusCode,
+    APIRequestError,
 )
 
 load_dotenv()
@@ -39,6 +41,11 @@ RETRY_PERIOD = 600
 PAUSE_BETWEEN_ITERATION = 3
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+DATA_FOR_REQUEST = {
+    'url': ENDPOINT,
+    'header': HEADERS,
+    'params': {'from_date': 0}
+}
 
 
 HOMEWORK_VERDICTS = {
@@ -50,38 +57,59 @@ HOMEWORK_VERDICTS = {
 
 def check_tokens():
     """Проверка токенов."""
-    try:
-        if not all([PRACTICUM_TOKEN, TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]):
-            raise InvalidTokenError
+    tokens = (
+        ('PRACTICUM_TOKEN', PRACTICUM_TOKEN),
+        ('TELEGRAM_TOKEN', TELEGRAM_TOKEN),
+        ('TELEGRAM_CHAT_ID', TELEGRAM_CHAT_ID)
+    )
 
-    except InvalidTokenError:
-        logger.critical('Ошибка проверки токена.')
-        sys.exit()
+    invalid_tokens = [token for token, value in tokens if value is None]
+
+    if invalid_tokens:
+        message = f'Ошибка проверки токена: {", ".join(invalid_tokens)}.'
+        logger.critical(message)
+        raise InvalidTokenError(message)
 
 
 def send_message(bot: TeleBot, message: str):
     """Отправка сообщения о статусе проверки ДЗ в ТГ."""
     try:
+        logger.debug(f'Отправка сообщения в чат: {TELEGRAM_CHAT_ID}...')
         bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
-        logger.debug('Сообщение успешно отправлено!')
-    except SendMessageError:
-        logger.error('Возникла ошибка при отправки сообщения...')
+        logger.debug(
+            f'Сообщение в чат: {TELEGRAM_CHAT_ID} успешно отправлено!'
+        )
+        return True
+    except apihelper.ApiException as error:
+        logger.error(f'Возникла ошибка при отправки сообщения... {error}')
+        return False
 
 
 def get_api_answer(timestamp):
     """Отправка API запроса."""
     try:
-        response = requests.get(
-            ENDPOINT, headers=HEADERS, params={'from_date': timestamp}
+        DATA_FOR_REQUEST['params']['from_date'] = timestamp
+
+        logger.debug(
+            'Отправка запроса на адрес: {url}. '
+            'Заголовок: {header}. '
+            'Параметры: {params} '.format(**DATA_FOR_REQUEST)
         )
-        logger.debug(f'Отправка запроса на адрес: {ENDPOINT}')
+        response = requests.get(
+            url=DATA_FOR_REQUEST['url'],
+            headers=DATA_FOR_REQUEST['header'],
+            params=DATA_FOR_REQUEST['params']
+        )
+
+        logger.debug('Ответ от: {url} получен.'.format(**DATA_FOR_REQUEST))
+
     except requests.RequestException as error:
-        logger.error(f'Ошибка при работе с Эндпоинтом: {ENDPOINT}. '
-                     f'Ошибка: {error}.')
+        raise APIRequestError(f'Ошибка {error} при обращении к {ENDPOINT}.')
     if response.status_code != HTTPStatus.OK:
-        logger.error(f'Эндпоинт: {ENDPOINT} не доступен. '
-                     f'Код ошибки: {response.status_code}.')
-        raise requests.RequestException
+        raise InvalidStatusCode(
+            'API-ответ был получен, но что-то пошло не так...'
+            f'Код ответа API: {response.status_code}.'
+        )
 
     return response.json()
 
@@ -89,32 +117,29 @@ def get_api_answer(timestamp):
 def check_response(response: dict):
     """Проверка структуры API-ответа."""
     if not isinstance(response, dict):
-        logger.error('Ошибка в структуре API-ответа.')
-        raise TypeError
-    elif 'homeworks' not in response:
-        logger.error('Отсутствует ключ homework в API-ответе.')
-        raise KeyError
-    elif not isinstance(response['homeworks'], list):
-        logger.error('Ошибка в структуре homework в API-ответа.')
-        raise TypeError
-    else:
-        return True
+        raise TypeError('Ошибка в структуре API-ответа.')
+    if 'homeworks' not in response:
+        raise KeyError('Отсутствует ключ homeworks в API-ответе.')
+    result = response['homeworks']
+    if not isinstance(result, list):
+        raise TypeError('Ошибка в структуре homeworks в API-ответа.')
+
+    return result
 
 
 def parse_status(homework: dict):
     """Генерация сообщения для отправки."""
     if 'homework_name' not in homework:
-        logger.error('Ошибка в названии ДЗ.')
         raise InvalidHomeWorkName('Ошибка в названии ДЗ.')
+
     if 'status' not in homework or homework['status'] not in HOMEWORK_VERDICTS:
-        logger.error('Ошибка в статусе ДЗ.')
         raise InvalidHomeWorkStatus('Ошибка в статусе ДЗ.')
 
     homework_name = homework['homework_name']
     verdict = HOMEWORK_VERDICTS[homework['status']]
 
     return (f'Изменился статус проверки работы "{homework_name}".'
-            f'Статус: {verdict}')
+            f' Статус: {verdict}')
 
 
 def main():
@@ -123,47 +148,35 @@ def main():
 
     bot = TeleBot(token=TELEGRAM_TOKEN)
     timestamp = int(time.time())
-    homeworks_stats = dict()
+    last_message = None
 
     while True:
         try:
-            response = get_api_answer(timestamp=timestamp)
-            if check_response(response):
-                homeworks = response['homeworks']
-                if len(homeworks) == 0:
-                    logger.debug('Список с домашними заданиями пуст...')
-                else:
-                    for homework in homeworks[::-1]:
-                        homework_name = homework['homework_name']
-                        homework_stat = homework['status']
-                        if homework_name in homeworks_stats:
-                            if (
-                                homework_stat == homeworks_stats[homework_name]
-                            ):
-                                logger.debug('Статус для домашнего задания '
-                                             f'{homework_name} не изменен...')
-
-                                continue
-                            else:
-                                text = parse_status(homework)
-                                send_message(bot, text)
-                                logger.debug('Статус для домашнего задания '
-                                             f'{homework_name} изменен. '
-                                             f'Статус: {homework_stat}.')
-                                homeworks_stats[homework_name] = homework_stat
-                        else:
-                            text = parse_status(homework)
-                            send_message(bot, text)
-                            logger.debug('Статус для домашнего задания '
-                                         f'{homework_name} изменен. '
-                                         f'Статус: {homework_stat}.')
-                            homeworks_stats[homework_name] = homework_stat
-                        time.sleep(PAUSE_BETWEEN_ITERATION)
-
+            response = get_api_answer(timestamp)
+            homeworks = check_response(response)
+            if not homeworks:
+                logger.debug('Список с домашними заданиями пуст...')
+            else:
+                homework_in_progress = homeworks[0]
+                text = parse_status(homework_in_progress)
+                is_msg_sent = send_message(bot, text)
+                if is_msg_sent:
+                    logger.debug(
+                        'Статус для домашнего задания '
+                        f'{homework_in_progress["homework_name"]} изменен. '
+                        f'Статус: {homework_in_progress["status"]}.'
+                    )
+                    new_timestamp = response.get('current_date')
+                    if new_timestamp:
+                        timestamp = new_timestamp
         except Exception as error:
             message = f'Сбой в работе программы: {error}'
-            logger.error(f'{message}')
-        time.sleep(RETRY_PERIOD)
+            logger.error(message)
+            if message != last_message:
+                send_message(bot, message)
+                last_message = message
+        finally:
+            time.sleep(RETRY_PERIOD)
 
 
 if __name__ == '__main__':
